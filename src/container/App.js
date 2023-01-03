d7265ad66f613250cc8a5678d4fc167f4fb17b90
dorigoluiz <luizdorigo01@gmail.com>
2023-01-02 21:10:20 -0300

    init: fix

---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "../assets/styles/reset.css";
+import "../assets/styles/style.css";
+
+function App() {
+    return (
+        <>
+            <>WEE</>
+        </>
+    );
+}
+
+export default App;
