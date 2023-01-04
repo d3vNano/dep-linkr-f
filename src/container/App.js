ea89a956be0f0ba3aa23ab426303bc08ca4a7cd9
Daniel <danielbellinijf21@gmail.com>
2023-01-04 12:29:22 -0300

    feat initial user page

--- src/container/App.js
+++ src/container/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../assets/styles/reset.css";
 import "../assets/styles/style.css";
 
+import UsersPage from "./users/users.page";
+
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <>WEE</>
-        </>
+            <Routes path="/user/:id" element={UsersPage}/>
+        </BrowserRouter>
     );
 }
 
