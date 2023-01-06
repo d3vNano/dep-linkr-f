d100309d8a5240ff09d1b311177f3a542e4d9628
Daniel <danielbellinijf21@gmail.com>
2023-01-06 11:33:43 -0300

    Function of search and render results of search in API

--- src/components/header/SearchBar.header.js
+++ src/components/header/SearchBar.header.js
@@ -1,44 +1,110 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {DebounceInput} from 'react-debounce-input';
+//import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function SearchBar (){
 
-//    exemplo
-//    const promisse = {
-//        users: [{
-//                    username: "João A",
-//                    id:21
-//                },{
-//                    username: "João B",
-//                    id:25
-//                },{
-//                    username: "João A",
-//                    id:29
-//            }]
-//    }
 
-    const [username, setUsername] = useState("");
+    const [search, setSearch] = useState("");
+    const [result, setResult] = useState([]);
     
-//    console.log(username)
+    useEffect(()=>{
+        async function getUsernameSearch(){
+            if(search.username){
+                try {
+                    const requisition = await axios.get(`http://localhost:4000/user/${search.username}`);
+                    setResult(requisition.data);
+                } catch (error) {
+                    console.log(error.response.data);
+                };
+            };
+        }
+        getUsernameSearch();
+    },[search])
+    
+    // function RenderUsernameResults({user_id, picture_url, username}){
+    //     return (
+    //             <Link key={user_id} to={`/user/${user_id}`}>
+    //                 <IconImage src={picture_url} alt={`picture at ${username}`}></IconImage>
+    //                 <UsernameBox className='username'>{username}</UsernameBox>
+    //             </Link>
+    //         )
+    // }
 
+
+    function RenderUsernameResults({user_id, picture_url, username}){
+        return (
+                <UsernameBox key={user_id}>
+                    <IconImage src={picture_url} alt={`picture at ${username}`}></IconImage>
+                    <span>{username}</span>
+                </UsernameBox>
+            )
+    }
+
+    
     return(
         <SearchBarLayout>
-            <DebounceInput
-                minLength={3}
-                debounceTimeout={300}
-                onChange={event => setUsername({username: event.target.value})} 
-            />
+            <SpaceFromSearch>
+                <SearchBox>
+                    <DebounceInput
+                        placeholder="Search for people and friends"
+                        minLength={3}
+                        debounceTimeout={300}
+                        onChange={event => setSearch({username: event.target.value})} 
+                        value={search.username}
+                    />
+                </SearchBox>
+                <ResultLayout> 
+                    {result.map(value=>{                       
+                        const {id, picture_url, username} = value
+
+                        return(
+                            <RenderUsernameResults  key={id}
+                                                    user_id={id}
+                                                    picture_url={picture_url}
+                                                    username={username}
+                        />)
+                    })}
+                </ResultLayout>
+            </SpaceFromSearch>
         </SearchBarLayout>
     )
 }
 
 const SearchBarLayout = styled.div`
 
+    width: 563px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const SpaceFromSearch = styled.div`
+    background = blue;
+
     input{
         width: 563px;
         height: 45px;
-        background: #FFFFFF;
-        border-radius: 8px;
+        background-color: #FFFFFF;
+
     }
+`
+const SearchBox = styled.div`
+    background= yellow;
+`
+const ResultLayout = styled.div`
+    background = red;
+`
+const UsernameBox = styled.div`
+    background = green
+`
+
+const IconImage = styled.img`
+    width: 53px;
+    height: 53px;
+
+    border-radius: 26.5px;
+    margin: 5px;
 `
